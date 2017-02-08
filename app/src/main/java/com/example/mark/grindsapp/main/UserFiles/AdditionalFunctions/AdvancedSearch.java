df1b34bbff53b6bd8776485887c899e02347d86e
Mark Gerrity <gerrity95@gmail.com>
2017-02-08 18:03:52 +0000

    Merge

---
+++ app/src/main/java/com/example/mark/grindsapp/main/UserFiles/AdditionalFunctions/AdvancedSearch.java
@@ -0,0 +1,7 @@
+package com.example.mark.grindsapp.main.UserFiles.AdditionalFunctions;
+
+/**
+ * Created by gerrity95 on 08/02/17.
+ */
+public class AdvancedSearch {
+}
---
+++ app/src/main/java/com/example/mark/grindsapp/main/UserFiles/TutorProfile.java
@@ -0,0 +1,7 @@
+package com.example.mark.grindsapp.main.UserFiles;
+
+/**
+ * Created by gerrity95 on 08/02/17.
+ */
+public class TutorProfile {
+}
