9aa1d382638f6d8feeaedc105724516338479c57
Mark Gerrity <gerrity95@gmail.com>
2017-02-10 16:39:52 +0000

    Removing some warnings

--- app/src/main/java/com/example/mark/grindsapp/main/UserFiles/Homescreen.java
+++ app/src/main/java/com/example/mark/grindsapp/main/UserFiles/Homescreen.java
@@ -4,7 +4,6 @@ import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.view.Menu;
